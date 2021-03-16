606385de221d56bd4753088a47065f377dfe8c82
bbaxteriv <bbaxteriv@gmail.com>
2021-03-16 11:54:30 -0700

    camera controls

--- Assets/Scripts/TileMapGenerator.cs
+++ Assets/Scripts/TileMapGenerator.cs
@@ -12,6 +12,8 @@ public class TileMapGenerator : MonoBehaviour
 
     List<int> tilePrefabIndex = new List<int>();
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
