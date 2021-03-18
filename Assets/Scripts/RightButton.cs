99ff3a71d2680426486aced461047b5eb5796b1f
bbaxteriv <bbaxteriv@gmail.com>
2021-03-18 11:48:10 -0700

    broken button

---
+++ Assets/Scripts/RightButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightButton : MonoBehaviour
+{
+
+    void OnMouseUp()
+    {
+        print("test");
+    }
+
+}
