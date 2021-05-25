90fae1eda17afdd1404e349c759c2271097e8382
bbaxteriv <bbaxteriv@gmail.com>
2021-05-25 09:01:34 -0700

    text update

--- Assets/Scripts/RobotMovement.cs
+++ Assets/Scripts/RobotMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Blocker;
 
 /*
@@ -10,6 +11,7 @@ Handles the movement of a user-controlled robot
 public class RobotMovement : ObjectMovement, IResettable
 {
     private RobotHealth robotHealth;
+    public Text textEdit;
 
     public string SequenceString = "Right, Left, Right, Left, Right,";
     /*
@@ -20,6 +22,7 @@ public class RobotMovement : ObjectMovement, IResettable
         base.Start();
         this.robotHealth = this.gameObject.GetComponent<RobotHealth>();
         this.selected = false;
+        textEdit = GameObject.Find("/Canvas/ResourceText").GetComponent<Text>();
     }
 
     /*
@@ -148,8 +151,12 @@ public class RobotMovement : ObjectMovement, IResettable
       {
         //Debug.Log("its a crate!");
         Destroy(other.gameObject);
-        //code needs to go here to increase scrap metal
-      } else if (other.ToString() == "Enemy (UnityEngine.BoxCollider2D)") {
+        
+        string currentResource = textEdit.text.Split(' ')[2];
+        textEdit.text = "Scrap Metal: " + (int.Parse(currentResource) + 3);
+
+            //code needs to go here to increase scrap metal
+        } else if (other.ToString() == "Enemy (UnityEngine.BoxCollider2D)") {
       //  Debug.Log("it's an enemy");
         robotHealth.TakeDamage(10);
       }
