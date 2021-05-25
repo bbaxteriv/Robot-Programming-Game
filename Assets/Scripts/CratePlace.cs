04f226156d57a8a1620fd67e2dcfca7caec52112
Jack0Spades <jackf22@lakesideschool.org>
2021-05-25 11:33:22 -0700

    crates now spawn randomly every minute or so

--- Assets/Scripts/CratePlace.cs
+++ Assets/Scripts/CratePlace.cs
@@ -10,10 +10,13 @@ public class CratePlace : MonoBehaviour
     [Range(0.0f,0.02f)]
     public float crateDensity;
 
+    [Range(0, 7200)]
+    public int framesTillNextCrate;
+    private int frameCounter;
     // Start is called before the first frame update
     void Start()
     {
-
+      frameCounter = framesTillNextCrate;
       this.mapGenerator = this.gameObject.GetComponent<TileMapGenerator>();
       System.Random random = new System.Random(this.mapGenerator.seed.GetHashCode());
       int areaOfMap = this.mapGenerator.rows*this.mapGenerator.columns;
@@ -41,6 +44,19 @@ public class CratePlace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+      frameCounter--;
+      if (frameCounter == 0) {
+          System.Random random = new System.Random();
+          while (true) {
+            int crateX = random.Next(0,this.mapGenerator.rows);
+            int crateY = random.Next(0,this.mapGenerator.columns);
+            if (this.mapGenerator.map[crateX,crateY]==0) {
+                Vector3 position = new Vector3(crateX, crateY-(float)0.4, 0);
+                GameObject Crate = Instantiate(CratePrefab, position, Quaternion.identity);
+                break;
+            }
+          }
+          frameCounter = framesTillNextCrate;
+      }
     }
 }
